408eb369eef17d5b0b5b6ddd06ce00ff32835430
Katie Bridson <kbridson@memphis.edu>
2024-02-05 15:16:19 -0600

    Updated output message string

--- check_multiple.py
+++ check_multiple.py
@@ -3,9 +3,9 @@ import random
 
 def check_multiple(number, divisor):
     if number % divisor == 0:
-        print(f"{number} is divisible by {divisor}")
+        print(f"{number} is evenly divisible by {divisor}")
     else:
-        print(f"{number} is not divisible by {divisor}")
+        print(f"{number} is evenly divisible by {divisor}")
 
 
 number = random.randint(1, 100)
